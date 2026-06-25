62d704932a7138a182e16a8a83778696a3dc1a2c
Amin <amin.nazari076@gmail.com>
2026-06-25 11:47:48 +1000

    Add studio module lifecycle

--- src/app/Studio.ts
+++ src/app/Studio.ts
@@ -1,17 +1,31 @@
 import type { StudioContext } from "./StudioContext";
+import type { StudioModule } from "./StudioModule";
 
 export class Studio {
   public readonly context: StudioContext;
+
+  private modules: StudioModule[] = [];
 
   constructor(context: StudioContext) {
     this.context = context;
   }
 
-  initialize() {
-    // Future home for manager startup.
+  register(module: StudioModule) {
+    this.modules.push(module);
+    module.initialize?.(this.context);
+  }
+
+  update(delta: number) {
+    this.modules.forEach((module) => {
+      module.update?.(delta);
+    });
   }
 
   dispose() {
-    // Future home for cleanup.
+    [...this.modules].reverse().forEach((module) => {
+      module.dispose?.();
+    });
+
+    this.modules = [];
   }
 }
