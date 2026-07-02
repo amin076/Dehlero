e9d98fb9a71e01d7eb8469aeb207de9d8f2df1af
Amin <amin.nazari076@gmail.com>
2026-07-02 15:58:57 +1000

    feat(runtime): start dehlero runtime server

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
-import "./style.css";
+﻿import "./style.css";
 import { createStudioApp } from "./app/createStudioApp";
+import { RuntimeServer } from "./runtime/server/RuntimeServer";
 
 createStudioApp({
   root: document.querySelector<HTMLDivElement>("#app")!,
 });
+
+const runtimeServer = new RuntimeServer(4010);
+runtimeServer.start();
